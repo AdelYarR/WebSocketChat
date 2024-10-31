c266377a24c6ca91dc281632ae409641c0a1abfb
Adel Yarullin <adeladelyar@gmail.com>
2024-10-31 21:13:44 +0300

    learning redis

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
+	"github.com/AdelYarR/WebSocketChat/config"
 	"github.com/AdelYarR/WebSocketChat/internal/ws"
+	"github.com/go-redis/redis/v8"
 )
 
 func main() {
+	cfg := config.MustLoad()
 	hubMap := make(map[string]*ws.Hub)
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     cfg.RedisAddr,
+		Password: "",
+		DB:       0,
+	})
+
+	ping, err := client.Ping(context.Background()).Result()
+	if err != nil {
+		fmt.Println("error connecting to redis: " + err.Error())
+		return
+	}
+
+	fmt.Println(ping)
+
+	err = client.Set(context.Background(), "name", "Test", 0).Err()
+	if err != nil {
+		fmt.Println("failed to set the value: " + err.Error())
+		return
+	}
+
+	val, err := client.Get(context.Background(), "name").Result()
+	if err != nil {
+		fmt.Println("failed to get the value: " + err.Error())
+		return
+	}
+
+	fmt.Printf("value retrieved from redis: %s\n", val)
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		ws.ServeWS(hubMap, w, r)
 	})
 
-	http.ListenAndServe("localhost:8080", nil)
+	http.ListenAndServe(cfg.BindAddr, nil)
 }
